a828d3ff867490ae40b49ab68b3e7e5b6398ea71
Mohamed Amine <mohamedaminebensafta@gmail.com>
2020-12-09 22:23:55 +0100

    debut projet version 3

---
+++ src/Entity/Cadeau.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CadeauRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=CadeauRepository::class)
+ */
+class Cadeau
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $contenu;
+
+    /**
+     * @ORM\Column(type="blob", nullable=true)
+     */
+    private $photo;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getContenu(): ?string
+    {
+        return $this->contenu;
+    }
+
+    public function setContenu(string $contenu): self
+    {
+        $this->contenu = $contenu;
+
+        return $this;
+    }
+
+    public function getPhoto()
+    {
+        return $this->photo;
+    }
+
+    public function setPhoto($photo): self
+    {
+        $this->photo = $photo;
+
+        return $this;
+    }
+}
